ca6f2a4296644b1f38480cb4832f3dc49405ed6d
Sasidhar Reddy Swarna Venkata Naga <Sasidhar Reddy Swarna Venkata Naga>
2016-06-17 21:42:31 -0400

    cal commit

---
+++ script.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+  var testNumLength = function(number) {
+    if (number.length > 9) {
+      totaldiv.text(number.substr(number.length - 9, 9));
+      if (number.length > 15) {
+        number = "";
+        totaldiv.text("Err");
+      }
+    }
+  };
+  var number = "";
+  var newnumber = "";
+  var operator = "";
+  var totaldiv = $("#total");
+  totaldiv.text("0");
+  $("#numbers > a").not("#clear,#clearall").click(function() {
+    number += $(this).text();
+    totaldiv.text(number);
+    testNumLength(number);
+  });
+  $("#operators > a").not("#equals").click(function() {
+    operator = $(this).text();
+    newnumber = number;
+    number = "";
+    totaldiv.text();
+  });
+  $("#clear,#clearall").click(function() {
+    number = "";
+    totaldiv.text("0");
+    if ($(this).attr("id") === "clearall") {
+      newnumber = "";
+    }
+  });
+  $("#equals").click(function() {
+    if (operator === "+") {
+      number = (parseInt(number, 10) + parseInt(newnumber, 10)).toString(10);
+    } else if (operator === "-") {
+      number = (parseInt(newnumber, 10) - parseInt(number, 10)).toString(10);
+    } else if (operator === "/") {
+      number = (parseInt(newnumber, 10) / parseInt(number, 10)).toString(10);
+    } else if (operator === "*") {
+      number = (parseInt(newnumber, 10) * parseInt(number, 10)).toString(10);
+    }
+    totaldiv.text(number);
+    testNumLength(number);
+
+  });
+  $(document).keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode === 49) {
+            $("#one").click();
+       } else if (keycode === 50) {
+            $("#two").click();
+        } else if (keycode === 51) {
+            $("#three").click();
+        } else if (keycode === 52) {
+            $("#four").click();
+        } else if (keycode === 53) {
+            $("#five").click();
+        } else if (keycode === 54) {
+            $("#six").click();
+        } else if (keycode === 55) {
+            $("#seven").click();
+        } else if (keycode === 56) {
+            $("#eight").click();
+        } else if (keycode === 57) {
+            $("#nine").click();
+        } else if (keycode === 48) {
+            $("#zero").click();
+        } else if (keycode === 97) {
+            $("#clearall").click();
+        } else if (keycode === 99) {
+            $("#clear").click();
+        } else if (keycode === 61) {
+            $("#equals").click();
+        } else if (keycode === 43) {
+            $("#plus").click();
+        } else if (keycode === 45) {
+            $("#minus").click();
+        } else if (keycode === 42 || keycode === 120) {
+            $("#multiply").click();
+        } else if (keycode === 47) {
+            $("#divide").click();
+        } 
+    });
+});
